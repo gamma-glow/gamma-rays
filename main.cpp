72a0ea6dcd8c838a617a71282f8d592b8a971fb6
gamma-glow <aditya2357patel@gmail.com>
2024-09-12 11:38:52 +0530

    learn about some common algos

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <algorithm>
+#include <numeric>
 
 int main(){
 
-    std::cout << "Hello World!" << endl;
+    int arr[] = {1,2,3,4,3,2,34,1};
+
+    std::reverse(arr,arr+8);
+    std::sort(arr,arr+8);
+    int cnt = std::count(arr,arr+8,2);
+    for(int i:arr){
+        std::cout << i <<"\n";
+    }
+    
     return 0;
 }
